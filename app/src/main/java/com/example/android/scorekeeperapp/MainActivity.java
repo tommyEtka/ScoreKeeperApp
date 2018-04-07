6ea17077087bc88e75db5341c5f5db3ec541f805
tommyEtka <thurma13@gmail.com>
2018-04-06 20:24:51 -0400

    add buttons

--- app/src/main/java/com/example/android/scorekeeperapp/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeperapp/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.android.scorekeeperapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamPistons=0;
+    int scoreTeamLakers=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +21,29 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = (TextView) findViewById(R.id.pistons_score);
         scoreView.setText(String.valueOf(score));
 
+    }
+
+    public void addOnePointForPistons(View view){
+        scoreTeamPistons = scoreTeamPistons +1;
+        displayForPistons(scoreTeamPistons);
+    }
+
+    public void displayForLakers(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.lakers_score);
+        scoreView.setText(String.valueOf(score));
+
+    }
+
+    public void addOnePointForLakers(View view){
+        scoreTeamLakers = scoreTeamLakers +1;
+        displayForLakers(scoreTeamLakers);
+    }
+
+
+    public void resetScores(View view){
+        scoreTeamLakers=0;
+        scoreTeamPistons = 0;
+        displayForLakers(scoreTeamLakers);
+        displayForPistons(scoreTeamPistons);
     }
 }
